2b4c154b88ade8b1109d6d25c69498b1f0a71c6c
Tom Tromey <tom@tromey.com>
2014-09-27 19:28:09 -0600

    rename to elcomp--can-throw

--- eh-cleanup.el
+++ eh-cleanup.el
@@ -1,26 +1,26 @@
 ;;; eh-cleanup.el - Clean up exceptions.
 
-(defgeneric elcomp--eh-nonlocal (insn)
+(defgeneric elcomp--can-throw (insn)
   "Return t if INSN can `throw' or `signal'."
   )
 
-(defmethod elcomp--eh-nonlocal (insn)
+(defmethod elcomp--can-throw (insn)
   "The base case is to assume any instruction can throw."
   t)
 
-(defmethod elcomp--eh-nonlocal ((insn elcomp--set))
+(defmethod elcomp--can-throw ((insn elcomp--set))
   nil)
 
-(defmethod elcomp--eh-nonlocal ((insn elcomp--goto))
+(defmethod elcomp--can-throw ((insn elcomp--goto))
   nil)
 
-(defmethod elcomp--eh-nonlocal ((insn elcomp--if))
+(defmethod elcomp--can-throw ((insn elcomp--if))
   nil)
 
-(defmethod elcomp--eh-nonlocal ((insn elcomp--return))
+(defmethod elcomp--can-throw ((insn elcomp--return))
   nil)
 
-(defmethod elcomp--eh-nonlocal ((insn elcomp--call))
+(defmethod elcomp--can-throw ((insn elcomp--call))
   ;; Note that we can't really be picky about `signal' or `throw'
   ;; tags, due to QUIT and `throw-on-input'.
   (if (and (symbolp (oref insn :func))
@@ -28,7 +28,7 @@
       nil
     t))
 
-(defmethod elcomp--eh-nonlocal ((insn elcomp--diediedie))
+(defmethod elcomp--can-throw ((insn elcomp--diediedie))
   t)
 
 (defun elcomp--eh-remove-unwinds (compiler bb)
@@ -61,7 +61,7 @@
        (when (elcomp--basic-block-exceptions bb)
 	 (unless (catch 'can-throw
 		   (dolist (insn (elcomp--basic-block-code bb))
-		     (when (elcomp--eh-nonlocal insn)
+		     (when (elcomp--can-throw insn)
 		       (throw 'can-throw t))))
 	   ;; Since nothing here can throw, we can remove the
 	   ;; exception handlers.
