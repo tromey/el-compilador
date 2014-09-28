22147e94ae516a73788271d463ac64fa3408e9bc
Tom Tromey <tom@tromey.com>
2014-09-27 19:50:05 -0600

    document back.el
    document back.el and also make
    elcomp--invalidate-back-edges actually clear the edges

--- back.el
+++ back.el
@@ -1,6 +1,15 @@
-;; Fix up back edges.
+;;; back.el - fix up back edges.
+
+;;; Commentary:
+
+;; Reconstruct the back edges in the CFG.
+
+;;; Code:
 
 (defun elcomp--reset-back-edges (compiler)
+  "Reset the back edges of all basic blocks in COMPILER.
+
+This sets all the back edges to nil."
   (elcomp--iterate-over-bbs
    compiler
    (lambda (bb)
@@ -8,22 +17,28 @@
 	   (make-hash-table)))))
 
 (defgeneric elcomp--add-links (insn block)
-  "Add backlinks")
+  "Add backlinks for the instruction INSN, which appears in BLOCK.")
 
 (defmethod elcomp--add-links (insn block)
+  "The base case does nothing.  Most instructions don't have links."
   ;; Do nothing.
   )
 
 (defmethod elcomp--add-links ((insn elcomp--goto) block)
+  "Add backlinks for a `goto'."
   (puthash block t (elcomp--basic-block-parents (oref insn :block))))
 
 (defmethod elcomp--add-links ((insn elcomp--if) block)
+  "Add backlinks for an `if'."
   (puthash block t (elcomp--basic-block-parents (oref insn :block-true)))
   (puthash block t (elcomp--basic-block-parents (oref insn :block-false))))
 
 (defun elcomp--require-back-edges (compiler)
+  "Require the back links in COMPILER to be valid.
+
+If the links are already believed to be valid, this does nothing.
+Otherwise, it recreates the links."
   (unless (elcomp--back-edges-valid compiler)
-    (elcomp--reset-back-edges compiler)
     (elcomp--iterate-over-bbs
      compiler
      (lambda (bb)
@@ -34,4 +49,9 @@
     (setf (elcomp--back-edges-valid compiler) t)))
 
 (defun elcomp--invalidate-back-edges (compiler)
-  (setf (elcomp--back-edges-valid compiler) nil))
+  "Invalidate the back links in COMPILER."
+  (when (elcomp--back-edges-valid compiler)
+    (elcomp--reset-back-edges compiler)
+    (setf (elcomp--back-edges-valid compiler) nil)))
+
+;;; back.el ends here
