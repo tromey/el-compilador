1cbe8ab015b5a381962960e1f78ed557fbaba683
Tom Tromey <tom@tromey.com>
2014-10-01 18:09:38 +0100

    make just-intrinsic an argument, not a slot

--- dce.el
+++ dce.el
@@ -15,69 +15,63 @@ It holds data internal to the pass."
   work-list
   ;; HASH is a hash table whose keys are instructions which have been
   ;; marked as needed.
-  (hash (make-hash-table))
-  ;; DCE proceeds in two phases.  In the first phase, only
-  ;; intrinsically necessary instructions are marked.  In the second
-  ;; phase, instructions which are needed by some other instruction
-  ;; are marked.  JUST-INTRINSIC is t in the first phase and nil in
-  ;; the second phase.
-  (just-intrinsic t))
+  (hash (make-hash-table)))
 
-(defgeneric elcomp--mark-necessary (insn dce)
+(defgeneric elcomp--mark-necessary (insn dce just-intrinsic)
   "Possibly mark the instruction INSN as necessary.
 DCE is the DCE state object for the pass.
 
-If the DCE 'just-intrinsic' slot is non-nil, then only mark the
-instruction if it is intrinsically needed.  If it is nil, then
-mark the instruction.
+If JUST-INTRINSIC is non-nil, then only mark the instruction if
+it is intrinsically needed.  If it is nil, then mark the
+instruction.
 
 Marking the instruction means adding it to the hash and then
 pushing the instruction's arguments onto the work list.")
 
-(defmethod elcomp--mark-necessary (insn dce)
+(defmethod elcomp--mark-necessary (insn dce just-intrinsic)
   "The default case is to mark a statement as needed."
   (puthash insn t (elcomp--dce-hash dce)))
 
-(defmethod elcomp--mark-necessary ((insn elcomp--return) dce)
+(defmethod elcomp--mark-necessary ((insn elcomp--return) dce just-intrinsic)
   "`If' statements are marked as needed and their argument is pushed."
   ;; An IF is always needed.
   (puthash insn t (elcomp--dce-hash dce))
   ;; And so is its reference.
   (push (oref insn :sym) (elcomp--dce-work-list dce)))
 
-(defmethod elcomp--mark-necessary ((insn elcomp--return) dce)
+(defmethod elcomp--mark-necessary ((insn elcomp--return) dce just-intrinsic)
   "`Return' statements are marked as needed and their argument is pushed."
   ;; A RETURN is always needed.
   (puthash insn t (elcomp--dce-hash dce))
   ;; And so is its reference.
   (push (oref insn :sym) (elcomp--dce-work-list dce)))
 
-(defmethod elcomp--mark-necessary ((insn elcomp--set) dce)
+(defmethod elcomp--mark-necessary ((insn elcomp--set) dce just-intrinsic)
   "Mark a `set' statement as necessary.
 
 In the first pass, do nothing.  A `set' is not intrinsically needed.
 In the second pass, mark this statement as needed, and then push
 its references on the work list."
   ;; A SET is not intrinsically needed, so check which pass this is.
-  (unless (elcomp--dce-just-intrinsic dce)
+  (unless just-intrinsic
     (puthash insn t (elcomp--dce-hash dce))
     (when (elcomp--ssa-name-p (oref insn :value))
       (push (oref insn :value) (elcomp--dce-work-list dce)))))
 
-(defmethod elcomp--mark-necessary ((insn elcomp--phi) dce)
+(defmethod elcomp--mark-necessary ((insn elcomp--phi) dce just-intrinsic)
   "Mark a `phi' statement as necessary.
 
 In the first pass, do nothing.  A `phi' is not intrinsically needed.
 In the second pass, mark this statement as needed, and then push
 its references on the work list."
   ;; A PHI is not intrinsically needed, so check which pass this is.
-  (unless (elcomp--dce-just-intrinsic dce)
+  (unless just-intrinsic
     (puthash insn t (elcomp--dce-hash dce))
     (maphash (lambda (arg _ignore)
 	       (push arg (elcomp--dce-work-list dce)))
 	     (oref insn :args))))
 
-(defmethod elcomp--mark-necessary ((insn elcomp--call) dce)
+(defmethod elcomp--mark-necessary ((insn elcomp--call) dce just-intrinsic)
   "Mark a `call' statement as necessary.
 
 In the first pass, do nothing.  A `call' is not intrinsically needed.
@@ -88,7 +82,7 @@ its references on the work list."
     ;; specially so we can determine whether its LHS is needed as
     ;; well.  Note that the "const" check also picks up the
     ;; "diediedie" statements.
-    (if (elcomp--dce-just-intrinsic dce)
+    (if just-intrinsic
 	(unless (elcomp--func-const-p (oref insn :func))
 	  (puthash insn :call (elcomp--dce-hash dce))
 	  (setf push-args t))
@@ -112,7 +106,7 @@ field of DCE and its references are pushed onto `work-list'."
    compiler
    (lambda (bb)
      (dolist (insn (elcomp--basic-block-code bb))
-       (elcomp--mark-necessary insn dce)))))
+       (elcomp--mark-necessary insn dce t)))))
 
 (defun elcomp--dce-propagate-necessary (compiler dce)
   "Propagate the \"necessary\" property through the function.
@@ -121,7 +115,6 @@ This is the second pass of DCE.
 
 This iterates over the work list, entering statements into the
 DCE's `hash' table and pushing references onto the `work-list'."
-  (setf (elcomp--dce-just-intrinsic dce) nil)
   (while (elcomp--dce-work-list dce)
     (let* ((insn (pop (elcomp--dce-work-list dce)))
 	   (mark (gethash insn (elcomp--dce-hash dce))))
@@ -131,7 +124,7 @@ DCE's `hash' table and pushing references onto the `work-list'."
 	  (when (eq mark :call)
 	    ;; Upgrade a call.
 	    (puthash insn t (elcomp--dce-hash dce)))
-	(elcomp--mark-necessary insn dce)))))
+	(elcomp--mark-necessary insn dce nil)))))
 
 (defun elcomp--dce-delete-dead-statements (compiler dce)
   "Delete dead statements.
