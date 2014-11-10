426d59298eddeb34c221a9947ffca9789c7f7fac
Tom Tromey <tom@tromey.com>
2014-11-10 01:28:27 -0700

    fix loadup to work on the second invocation

--- loadup.el
+++ loadup.el
@@ -7,23 +7,23 @@
   (interactive)
   (let ((load-path load-path))
     (push elcomp--loadup-dir load-path)
-    (dolist (feature '(elcomp
-		       elcomp/back
-		       elcomp/cmacros
-		       elcomp/coalesce
-		       elcomp/comp-debug
-		       elcomp/cprop
-		       elcomp/dce
-		       elcomp/dom
-		       elcomp/eh-cleanup
-		       elcomp/eltoc
-		       elcomp/iter
-		       elcomp/jump-thread
-		       elcomp/linearize
-		       elcomp/phiopt
-		       elcomp/props
-		       elcomp/ssa
-		       elcomp/subst
-		       elcomp/toplevel
-		       elcomp/typeinf))
-      (require feature))))
+    (dolist (file '("elcomp"
+		    "elcomp/back"
+		    "elcomp/cmacros"
+		    "elcomp/coalesce"
+		    "elcomp/comp-debug"
+		    "elcomp/cprop"
+		    "elcomp/dce"
+		    "elcomp/dom"
+		    "elcomp/eh-cleanup"
+		    "elcomp/eltoc"
+		    "elcomp/iter"
+		    "elcomp/jump-thread"
+		    "elcomp/linearize"
+		    "elcomp/phiopt"
+		    "elcomp/props"
+		    "elcomp/ssa"
+		    "elcomp/subst"
+		    "elcomp/toplevel"
+		    "elcomp/typeinf"))
+      (load file))))
